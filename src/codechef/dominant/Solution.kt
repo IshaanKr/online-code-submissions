c2a7b27ce2277e83ad01a89fce6efb30bda648ad
Ishaan Kumar <i.k.ishaan786@gmail.com>
2022-05-15 20:50:46 +0530

    fctrl2,dominant done

---
+++ src/codechef/dominant/Solution.kt
@@ -0,0 +1,20 @@
+package codechef.dominant
+
+import java.util.Scanner
+
+//Driver Code
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val loop = scanner.nextInt()
+    repeat(loop) {
+        println(resultOfThreeInput(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()))
+    }
+}
+
+//Main Logic
+fun resultOfThreeInput(i1: Int, i2: Int, i3: Int): String {
+    if (i1 > i2 + i3) return "yes"
+    if (i2 > i3 + i1) return "yes"
+    if (i3 > i2 + i1) return "yes"
+    return "no"
+}
---
+++ src/codechef/fctrl2/Solution.kt
@@ -0,0 +1,22 @@
+package codechef.fctrl2
+
+import java.math.BigInteger
+import java.util.*
+
+//Driver Code
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val loop = scanner.nextInt()
+    repeat(loop) {
+        println(resultOfOneInput(scanner.nextInt()))
+    }
+}
+
+//Main Logic
+fun resultOfOneInput(i1: Int): String {
+    var result = BigInteger.ONE
+    for (it in 1..i1) {
+        result = result.multiply(BigInteger(it.toString()))
+    }
+    return result.toString()
+}
