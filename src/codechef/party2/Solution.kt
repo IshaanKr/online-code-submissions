9557bf8124de512aef7d0fc816221b99c470dccf
Ishaan Kumar <i.k.ishaan786@gmail.com>
2022-05-15 20:24:39 +0530

    party2 done

---
+++ src/codechef/party2/Solution.kt
@@ -0,0 +1,17 @@
+package codechef.party2
+
+import java.util.Scanner
+
+//Driver Code
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val loop = scanner.nextInt()
+    repeat(loop) {
+        println(resultOfThreeInput(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()))
+    }
+}
+
+//Main Logic
+fun resultOfThreeInput(i1: Int, i2: Int, i3: Int): String {
+    return if (i1 * i2 <= i3) return "yes" else "no"
+}
