b2fa45069376a9b62b161382dd85e5d690744e59
Ishaan Kumar <i.k.ishaan786@gmail.com>
2022-05-15 20:16:20 +0530

    added three argument answer

--- src/codechef/base/Solution.kt
+++ src/codechef/base/Solution.kt
@@ -2,13 +2,20 @@ package codechef.base
 
 import java.util.Scanner
 
+//Driver Code
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     val loop = scanner.nextInt()
     repeat(loop){
         println(resultOfOneInput(scanner.nextInt()))
         println(resultOfTwoInput(scanner.nextInt(), scanner.nextInt()))
+        println(resultOfThreeInput(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()))
     }
+}
+
+//Main Logic
+fun resultOfThreeInput(i1: Int, i2: Int, i3:Int): String {
+    return "Hello World"
 }
 
 fun resultOfTwoInput(i1: Int, i2: Int): String {
