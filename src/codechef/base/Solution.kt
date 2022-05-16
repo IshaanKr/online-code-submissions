c5e8314d3a7c408a1afd9a02358209921743d993
Ishaan Kumar <i.k.ishaan786@gmail.com>
2022-05-16 12:26:27 +0530

    base solution

--- src/codechef/base/Solution.kt
+++ src/codechef/base/Solution.kt
@@ -6,15 +6,23 @@ import java.util.Scanner
 fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
     val loop = scanner.nextInt()
-    repeat(loop){
-        println(resultOfOneInput(scanner.nextInt()))
-        println(resultOfTwoInput(scanner.nextInt(), scanner.nextInt()))
-        println(resultOfThreeInput(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()))
+    repeat(loop) {
+        val noOfInput = scanner.nextInt()
+        repeat(noOfInput) {
+            println(resultOfOneInput(scanner.nextInt()))
+            println(resultOfTwoInput(scanner.nextInt(), scanner.nextInt()))
+            println(resultOfThreeInput(scanner.nextInt(), scanner.nextInt(), scanner.nextInt()))
+            println(resultOfFourInput(scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt()))
+        }
     }
 }
 
+fun resultOfFourInput(i1: Int, i2: Int, i3: Int, i4: Int): String {
+    return "Hello World"
+}
+
 //Main Logic
-fun resultOfThreeInput(i1: Int, i2: Int, i3:Int): String {
+fun resultOfThreeInput(i1: Int, i2: Int, i3: Int): String {
     return "Hello World"
 }
 
---
+++ src/codechef/base/Solution.py
@@ -0,0 +1,6 @@
+T = int(input())
+
+for t in range(0, T):
+    N = int(input())
+    for n in range(0, N):
+        i1, i2, i3, i4 = map(int, input().split())
