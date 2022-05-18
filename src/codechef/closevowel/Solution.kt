3bcf332d35fcd78b17d7cd37fa508cb20ac3632a
Ishaan Kumar <i.k.ishaan786@gmail.com>
2022-05-19 00:54:01 +0530

    added more submissions

---
+++ src/codechef/closevowel/Solution.kt
@@ -0,0 +1,33 @@
+package codechef.closevowel
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    val loop = scanner.nextInt()
+    repeat(loop) {
+        val w = scanner.nextInt()
+        println(solve(scanner.next()!!))
+
+    }
+}
+
+fun solve(next: String): Int {
+    val count = next.count {
+        it == 'c' || it == 'g' || it == 'l' || it == 'r'
+    }
+    if (count == 0) return 1
+    return twoKiPowerModulo(count)
+}
+
+fun twoKiPowerModulo(power: Int): Int {
+    var ans = 1
+
+    val quesModulo = 1000000007
+
+    repeat(power) {
+        ans = ans shl 1
+        ans %= quesModulo
+    }
+    return ans
+}
